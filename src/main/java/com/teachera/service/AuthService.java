09709ec2bf82be1fc46eb7385172095b8a43d98b
Lydia-dev-eth <brhanemeskellidu@gmail.com>
2026-01-29 22:40:44 +0300

    auth service

--- src/main/java/com/teachera/service/AuthService.java
+++ src/main/java/com/teachera/service/AuthService.java
@@ -1,3 +1,20 @@
-public class AuthService {
-    
+package com.teachera.service;
+
+import com.teachera.model.Role;
+import com.teachera.model.User;
+
+import java.util.Optional;
+
+public interface AuthService {
+
+    Optional<User> login(String username, String password);
+
+    Optional<User> getCurrentUser();
+
+    void logout();
+
+    boolean hasRole(Role role);
+
+    java.util.Optional<com.teachera.model.User> register(String username, String password);
+
 }
