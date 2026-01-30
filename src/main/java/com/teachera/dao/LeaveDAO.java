3087a7b535e90812a83971172e213e2fb543fd56
mahi-2121 <Mahlet15tadese@gmail.com>
2026-01-30 11:46:40 +0300

    leave DAO

--- src/main/java/com/teachera/dao/LeaveDAO.java
+++ src/main/java/com/teachera/dao/LeaveDAO.java
@@ -1,3 +1,24 @@
-public class LeaveDAO {
-    
+package com.teachera.dao;
+
+import com.teachera.model.Leave;
+import com.teachera.model.LeaveStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LeaveDAO {
+
+    void save(Leave leave);
+
+    void update(Leave leave);
+
+    void deleteById(int id);
+
+    Optional<Leave> findById(int id);
+
+    List<Leave> findByTeacher(int teacherId);
+
+    List<Leave> findByStatus(LeaveStatus status);
+
+    List<Leave> findAll();
 }
