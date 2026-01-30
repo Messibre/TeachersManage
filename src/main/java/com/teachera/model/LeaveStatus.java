97ba0fb84219cd92bf77523e0721f95b61e01924
mahi-2121 <Mahlet15tadese@gmail.com>
2026-01-30 11:50:14 +0300

    leave models

---
+++ src/main/java/com/teachera/model/LeaveStatus.java
@@ -0,0 +1,7 @@
+package com.teachera.model;
+
+public enum LeaveStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
---
+++ src/main/java/com/teachera/model/LeaveType.java
@@ -0,0 +1,9 @@
+package com.teachera.model;
+
+public enum LeaveType {
+    SICK,
+    CASUAL,
+    ANNUAL,
+    UNPAID,
+    OTHER
+}
